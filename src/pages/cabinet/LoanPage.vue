6513ef562f5aabd8e842fa013d011bda09dacc79
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-08-09 14:26:36 +0500

    Update LoanPage.vue

--- src/pages/cabinet/LoanPage.vue
+++ src/pages/cabinet/LoanPage.vue
@@ -1,10 +1,29 @@
 <script setup>
-import { useRoute } from "vue-router";
-const route = useRoute();
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+const store = useAuthStore();
+const router = useRouter();
+const loansList = store.user.loans;
+const loansActiveList = loansList.length ? loansList.filter((loan) => loan.active) : [];
+if (!loansList.length) {
+	router.push({ name: store.user.step });
+}
 </script>
 
 <template>
-	<h1 class="text-3xl font-bold">{{ route?.meta?.title }}</h1>
+	<div class="" v-if="loansList.length"></div>
+	<div class="" v-else-if="loansActiveList.length"></div>
+	<div class="" v-else>
+		<p>
+			У Вас нет активного займа,
+			<router-link
+				class="underline text-lime-600 hover:no-underline"
+				:to="{ name: 'cabinet.process' }"
+				>оформите</router-link
+			>
+			прямо сейчас!
+		</p>
+	</div>
 </template>
 
 <style></style>
